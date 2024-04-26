7c6e51a4c329ab09d5678d178420ec6bcfad4993
Corrado Baccheschi <79322723+Bakko000@users.noreply.github.com>
2024-04-26 19:12:27 +0200

    update

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,5 +1,6 @@
 
 import re
+import csv
 
 def get_split(file_path):
     if 'training' in file_path:
@@ -13,3 +14,47 @@ def get_doc_info(header):
     genre = re.findall(r'genre="(.*?)"', header)[0]
     gender = re.findall(r'gender="(.*?)"', header)[0]
     return doc_id, genre, gender
+
+
+def load_dataset(src_path):
+    features_names = None
+    documents_info = []
+    features = []
+    with open(src_path, 'r') as src_file:
+        csv_reader = csv.reader(src_file, delimiter='\t')
+        for row in csv_reader:
+            if features_names is None: # la prima riga del csv contiene 'Filename' e tutti i nomi delle features
+                features_names = row[1:]
+            else:
+                documents_info.append(row[0]) # il primo elemento di ogni riga contiene il nome del file, dove sono codificati lo split e le feature
+                features.append(row[1:]) # dal secondo elemento in poi ci sono le features
+    return features_names, documents_info, features
+
+
+def create_label_list(documents_info):
+    labels = []
+    for doc_info in documents_info:
+
+        doc_info = doc_info[0:-len('.conllu')]
+        splitted_doc_info = doc_info.split('#')
+
+        genre = splitted_doc_info[2]
+        gender = splitted_doc_info[3]
+
+        labels.append(gender)
+    return labels
+
+
+def train_test_split(documents_info, features, labels):
+    train_features, test_features = [], []
+    train_labels, test_labels = [], []
+
+    for doc_info, doc_features, doc_label in zip(documents_info, features, labels): # for idx in range(len(documents_info)):
+        if 'training' in doc_info:
+            train_features.append(doc_features)                                     # train_features.append(documents_info[idx])
+            train_labels.append(doc_label)
+        else: # if 'test' in file_name
+            test_features.append(doc_features)
+            test_labels.append(doc_label)
+
+    return train_features, train_labels, test_features, test_labels
